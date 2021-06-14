3f5e769fc91490ab47ca00e574ae43dab3dac85d
nicollas Marinho <cartola1320@gmail.com>
2021-06-14 11:00:36 -0300

    implementação de arquivos estáticos

--- core/views.py
+++ core/views.py
@@ -1,9 +1,9 @@
-
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     context = {
-        'title': 'Django E-Commerce'
+        'title': "django e-ecommerce"
     }
     return render(request, 'index.html', context)
