270d434d1a3b0a18cf4f53187e784cf13ab8fea0
nicollas Marinho <cartola1320@gmail.com>
2021-06-15 15:21:54 -0300

    settings update

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'django-insecure-&6rj&ds@!e1jx=f+5u-m3g@@3(9zorep-3jbul81d+%mkf+v$6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
