d1a97f6e8bea68852f3bf71968a2257160fd16b0
nicollas Marinho <cartola1320@gmail.com>
2021-06-23 12:00:06 -0300

    implementing final catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,4 +6,5 @@ app_name = 'product_list'
 urlpatterns = [
     url(r'^$', views.product_list, name='product_list'),
     url(r'^(?P<slug>[\w_-]+)/$', views.category, name='category'),
+    url(r'^produtos/(?P<slug>[\w_-]+)/$', views.product, name='product'),
 ]
